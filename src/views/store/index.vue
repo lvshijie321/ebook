605628531447ac0835758765463853a071461be2
lvshijie <lvshijie@aiwending.com>
2019-04-10 21:08:15 +0800

    '.'

---
+++ src/views/store/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="store">
+      <router-view />
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name: "storeIndex",
+
+    }
+</script>
+
+<style lang="scss" scoped>
+  .store {
+    background-color: white;
+    width: 100%;
+    height: 100%;
+  }
+</style>
